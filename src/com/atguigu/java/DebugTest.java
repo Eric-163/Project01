b880bc28a7a561418fbd4cfd27adf54ea28f44bf
liuhuan_pro <15966685681@163.com>
2020-02-26 20:51:58 +0800

    Debug Test

---
+++ src/com/atguigu/java/DebugTest.java
@@ -0,0 +1,28 @@
+package com.atguigu.java;
+
+import java.util.HashMap;
+
+/**
+ * @author liuhuan
+ * @create 2020-02-26 19:38
+ */
+public class DebugTest {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+
+        }
+        HashMap<String, String> map = new HashMap<>();
+        map.put("name","Tom");
+        map.put("age","12");
+        map.put("school","Tsinghua");
+        map.put("major","computer");
+
+        String age = map.get("age");
+        System.out.println("age = " + age);
+
+        map.remove("major");
+        System.out.println(map);
+
+    }
+}
